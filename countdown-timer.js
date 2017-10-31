e36cfd97d4f43b3910d705dd8acd9011c4a83745
CosmoDev90 <martin.c90@live.co.uk>
2017-10-31 13:56:00 +0000

    format output for css classes

--- countdown-timer.js
+++ countdown-timer.js
@@ -6,6 +6,10 @@ const _hour = _minute * 60;
 const _day = _hour * 24;
 let timer = '';
 
+function spanFormat(time, text) {
+    return '<span class="count">' + time + '</span>' + ' ' + '<span class="txt">' + text + '</span>';
+}
+
 function showRemaining() {
     const now = new Date();
     const distance = end - now;
@@ -21,10 +25,10 @@ function showRemaining() {
     const minutes = Math.floor((distance % _hour) / _minute);
     const seconds = Math.floor((distance % _minute) / _second);
 
-    document.getElementById('countdown').innerHTML = days + 'days ';
-    document.getElementById('countdown').innerHTML += hours + 'hrs ';
-    document.getElementById('countdown').innerHTML += minutes + 'mins ';
-    document.getElementById('countdown').innerHTML += seconds + 'secs';
+    document.getElementById('countdown').innerHTML = spanFormat(days, 'days ');
+    document.getElementById('countdown').innerHTML += spanFormat(hours, 'hrs ');
+    document.getElementById('countdown').innerHTML += spanFormat(minutes, 'mins ');
+    document.getElementById('countdown').innerHTML += spanFormat(seconds, 'secs');
 }
 
 timer = setInterval(showRemaining, 1000);
